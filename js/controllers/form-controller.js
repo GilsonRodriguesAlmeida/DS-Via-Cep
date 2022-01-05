1be07c795babe82da462f104761d4e24bb7873b8
Gilson Rodrigues de Almeida <gilson.almeida@al.infnet.edu.br>
2022-01-05 07:06:13 -0300

    Implementação das logícas de validação e controles

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,4 +1,6 @@
 import Address from '../models/address.js';
+import * as addressService from '../services/address-service.js';
+import * as listController from './list-controller.js'
 
 function State() {
 
@@ -32,6 +34,54 @@ export function init() {
     state.errorNumber = document.querySelector('[data-error="number"]');
 
     state.inputNumber.addEventListener('change', handleInputNumberChange);
+    state.inputNumber.addEventListener('keyup', handleInputNumberKeyup);
+    state.btnClear.addEventListener('click', handleBtnClearClick);
+    state.btnSave.addEventListener('click', handleBtnSaveClick);
+
+    state.inputCep.addEventListener('change', handleInputCepChange);
+}
+
+// captura numero de endereço digita pelo usuario e atribui ao inputNumber
+function handleInputNumberKeyup(event) {
+    state.address.number = event.target.value;
+}
+
+async function handleInputCepChange(event) {
+    const cep = event.target.value;
+
+    try {
+        const address = await addressService.findByCep(cep);
+
+        state.inputCity.value = address.city;
+        state.inputStreet.value = address.street;
+        state.address = address;
+
+        setFormError("cep", "");
+        state.inputNumber.focus();
+    }
+    catch (e) {
+        state.inputStreet.value = "";
+        state.inputCity.value = "";
+        setFormError("cep", "Informe um CEP válido!");
+    }
+}
+
+function handleBtnSaveClick (event) {
+    event.preventDefault();
+
+    const errors = addressService.getErrors(state.address);
+
+    const keys = Object.keys(errors);
+    
+    if(keys.length > 0) {
+        for(let i = 0; i < keys.length; i++) {
+            setFormError(keys[i], errors[keys[i]]);
+        }
+    }
+    else {
+        listController.addCard(state.address);
+        clearForm();
+    }
 }
 
 function handleInputNumberChange(event) {
@@ -41,6 +91,24 @@ function handleInputNumberChange(event) {
     else {
         setFormError("number", "");
     }
+}
+
+function handleBtnClearClick(event) {
+    event.preventDefault();
+    clearForm();
+}
+
+function clearForm() {
+    state.inputCep.value = "";
+    state.inputStreet.value = "";
+    state.inputNumber.value = "";
+    state.inputCity.value = "";
+
+    setFormError("cep", "");
+    setFormError("number", "");
+
+    state.address = new Address();
+    state.inputCep.focus();
 }
 
 function setFormError(key, value) {
